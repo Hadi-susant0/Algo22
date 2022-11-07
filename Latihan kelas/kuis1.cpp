d51b15f169547573325e6d5f7fd23037043eda57
Hadi-susant0 <hadi.s256314@uvers.ac.id>
2022-11-07 13:35:16 +0700

    kuis
    latihan kuis

---
+++ Latihan kelas/kuis1.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+	/*int a= 4, b=8;
+	for(b=1; b<=3; b++){
+		b = b + a;
+	}
+	cout<<b<<endl;
+	
+	/*int c=10, d;
+	for(d=c; c<10; c--){
+		cout<<d<<" ";
+	}*/
+	
+	int a = 50, b, e;
+	a = a % 9; b =a - 2;
+	cout<<a<<endl;
+	cout<<b<<endl;
+	
+	a++;--b;
+	cout<<a<<endl;
+	cout<<b<<endl;
+	
+	e = a>b;
+	cout<<e<<endl;/*
+	
+	int a=10,b=3;
+	cout<<"~"<<a<<" = "<<(~a)<<endl;*/
+
+}
